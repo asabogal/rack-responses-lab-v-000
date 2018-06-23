7e96d61b8f1554007107375ae9a8e080ca2af626
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-23 15:58:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,17 @@ class Application
 
   def call
     resp = Rack::Response.new
-    resp.write "Good Morning"
+
+      hour = Kernel.rand(1..24)
+
+      if hour >= 12
+        resp.write "Good Morning"
+      else
+        resp.write "Good Afternoon"
+      end
+
+    resp.finish
+
   end
 
 end
